d846db55d0d28e0b08300d97df62177e43d0b625
Péter Kincses <peter.kincsesj@gmail.com>
2021-03-17 12:03:04 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Matrix m = new Matrix(6,7);
+        System.out.println(m);
+        System.out.println("\n");
+        Matrix m2 = new Matrix(8,8);
+        System.out.println(m2.matrixSum());
+    }
+}
---
+++ src/Matrix.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+
+public class Matrix {
+    public Matrix(int sor, int oszlop) {
+        this.sor = sor;
+        this.oszlop = oszlop;
+        this.m = new  int[sor][oszlop];
+        for (int i = 0; i < sor; i++) {
+            for (int j = 0; j < oszlop; j++) {
+                m[i][j] = rnd.nextInt(100);
+            }
+        }
+    }
+
+    public int getSor() {
+        return sor;
+    }
+
+    public void setSor(int sor) {
+        this.sor = sor;
+    }
+
+    public int getOszlop() {
+        return oszlop;
+    }
+
+    public void setOszlop(int oszlop) {
+        this.oszlop =oszlop;
+    }
+    public int[][] getM() {
+        return m;
+    }
+
+    public void setM(int[][] m) {
+        this.m = m;
+    }
+    
+    private int sor;
+    private int oszlop;
+    private int [][] m ;
+    static Random rnd= new Random();
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                s+= String.format("%3d",m[i][j]);
+            }
+            s+=System.lineSeparator();
+        }
+        return s ;
+    }
+
+    public int matrixSum(){
+        int osszeg = 0;
+        for (int[] matrixsor : m) {
+            for (int szam: matrixsor
+                 ) {
+                osszeg+=szam;
+            }
+        }
+        return osszeg;
+    }
+}
